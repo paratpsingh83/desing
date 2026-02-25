e555e99f432a824de08dbbd8e3e4f4c07b4a9859
Pratap Singh <pratapsingh839399@gmail.com>
2026-02-26 00:43:39 +0800

    Builder  design pattern

---
+++ src/Creational/builder_design_patter/Main.java
@@ -0,0 +1,21 @@
+package Creational.builder_design_patter;
+
+public class Main {
+    public static void main(String[] args) {
+
+        User user = new User.UserBuilder()
+                .setUserId("123")
+                .setUserName("titu")
+                .setEmailId("titu@gmail.com")
+                .build();
+
+        System.out.println(user);
+
+        User user1 = User.UserBuilder.builder()
+                .setEmailId("titu123")
+                .setUserName("titu123")
+                //mising fields
+                .build();
+        System.out.println(user1);
+    }
+}
